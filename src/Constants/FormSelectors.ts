ab87a0632238c8835fa4be0dd6320007072233cc
SiiR-GH <matt_live@live.co.uk>
2023-04-07 15:20:20 +0100

    adding constants

---
+++ src/Constants/FormSelectors.ts
@@ -0,0 +1,11 @@
+export const SIGNIN = {
+  HEADING: 'Already have an account?',
+  SUBHEADING: 'Sign in and continue your journey',
+  CTA: 'Sign in',
+};
+
+export const SIGNUP = {
+  HEADING: 'Start your journey now',
+  SUBHEADING: "Don't have an account? Sign up and start your journey today!",
+  CTA: 'Sign up',
+};
